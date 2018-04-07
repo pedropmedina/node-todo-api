2def78cb4940ea7779d9d32db9e35c509e08023b
pedropmedina <pedrodelilia@gmail.com>
2018-04-07 01:30:22 -0400

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 // The line below tells mongoose to user the global promises that come
 // with javascript
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(
+	process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
+);
 
 module.exports = {
 	mongoose,
